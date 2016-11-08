c52285bf788ee3c6823e974e30f2df1d25600fa1
Romain Sumerot <romain.sumerot@etu.unice.fr>
2016-11-08 10:51:03 +0100

    Try to push

---
+++ src/main/java/fr/unice/polytech/qgl/iaad/Explorer.java
@@ -0,0 +1,27 @@
+package fr.unice.polytech.qgl.iaad;
+
+import eu.ace_design.island.bot.IExplorerRaid;
+
+
+public class Explorer implements IExplorerRaid {
+
+    @Override
+    public void initialize(String s) {
+         throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public String takeDecision() {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public void acknowledgeResults(String s) {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public String deliverFinalReport() {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+}
