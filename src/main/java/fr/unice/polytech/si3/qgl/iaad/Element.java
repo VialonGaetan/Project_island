71797142af4c76a4841f4546b4efec5361d45da3
Romain Sumerot <romain.sumerot@etu.unice.fr>
2016-11-15 22:54:51 +0100

    added Element enum wich contains informations like OCEAN, CREEK, EMERGENCY_SITE, ...

---
+++ src/main/java/fr/unice/polytech/si3/qgl/iaad/Element.java
@@ -0,0 +1,18 @@
+package fr.unice.polytech.si3.qgl.iaad;
+
+/**
+ * Created by romain on 15/11/16.
+ */
+
+/*
+ * Contains Elements of the map
+ */
+public enum Element
+{
+    OCEAN,
+    CREEK,
+    EMERGENCY_SITE,
+    BEACH,
+    WOOD,
+    UNKNOWN;
+}
