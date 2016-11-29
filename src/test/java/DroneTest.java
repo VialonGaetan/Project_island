4c8f2cfbef33d080608e2f666b4721abc70b192a
Theo Cholley <theo.cholley@etu.unice.fr>
2016-11-29 22:50:26 +0100

    début Test Drone

---
+++ src/test/java/DroneTest.java
@@ -0,0 +1,31 @@
+import fr.unice.polytech.si3.qgl.iaad.Direction;
+import fr.unice.polytech.si3.qgl.iaad.actions.Stop;
+import fr.unice.polytech.si3.qgl.iaad.aerial.Drone;
+import fr.unice.polytech.si3.qgl.iaad.islandMap.IslandMap;
+import org.junit.Before;
+import fr.unice.polytech.si3.qgl.iaad.result.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Théo
+ */
+public class DroneTest {
+
+    int budget;
+    Direction direction;
+    IslandMap map;
+    Drone drone;
+
+    @Test
+    public void doActionTest()
+    {
+        direction = Direction.E;
+        map = new IslandMap();
+        drone = new Drone(100,direction,map);
+        Stop stop = new Stop();
+        assertTrue(drone.doAction().getClass().isInstance(stop));
+    }
+
+}
