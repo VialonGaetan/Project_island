9278cae418b7d5f5412a37027dd80f9338f071cf
Theo Cholley <theo.cholley@etu.unice.fr>
2017-02-27 20:49:44 +0100

    update second contrat (test en cours) ticket IAAD-84 ticket IAAD-101 ticket IAAD-102

--- src/test/java/fr/unice/polytech/si3/qgl/iaad/util/contract/SecondContractTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/iaad/util/contract/SecondContractTest.java
@@ -1,16 +1,21 @@
 package fr.unice.polytech.si3.qgl.iaad.util.contract;
 
 import fr.unice.polytech.si3.qgl.iaad.engine.format.Context;
+import fr.unice.polytech.si3.qgl.iaad.util.map.Direction;
 import fr.unice.polytech.si3.qgl.iaad.util.resource.Resource;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Théo on 23/02/2017.
@@ -20,6 +25,7 @@ public class SecondContractTest {
     Context context;
     JSONObject firstContext;
     SecondContract secondContract;
+    private List<Contract> contracts;
 
     @Before
     public void defineContext() {
@@ -33,7 +39,23 @@ public class SecondContractTest {
                 "  ],\n" +
                 "  \"heading\": \"W\"\n" +
                 "}");
-        // context = new Context(firstContext);
+        //context = new Context(firstContext);
+
+    }
+
+    @Ignore
+    @Before
+    public void setUp() throws Exception
+    {
+        context = mock(Context.class);
+        contracts = new ArrayList<>();
+        //contracts.add(new Contract(Resource.FISH, 1000));
+        //contracts.add(new StandardContract(Resource.GLASS, 50));
+
+        when(context.getBudget()).thenReturn(10000);
+        when(context.getHeading()).thenReturn(Direction.E);
+        when(context.getNumberOfMen()).thenReturn(12);
+        when(context.getContracts()).thenReturn(contracts);
         secondContract = new SecondContract(context);
         this.secondContract.createSecondContract();
     }
