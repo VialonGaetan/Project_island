b475a78d740afb8d479aa3761b6693e255162f3b
Theo Cholley <theo.cholley@etu.unice.fr>
2017-03-02 16:32:14 +0100

    Modification des tests de Second contract suite au refactor. Ticket IAAD-84 Ticket IAAD-101 Ticket IAAD-102

--- src/test/java/fr/unice/polytech/si3/qgl/iaad/util/contract/SecondContractTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/iaad/util/contract/SecondContractTest.java
@@ -1,16 +1,21 @@
 package fr.unice.polytech.si3.qgl.iaad.util.contract;
 
+import fr.unice.polytech.si3.qgl.iaad.util.map.Direction;
 import fr.unice.polytech.si3.qgl.iaad.engine.format.Context;
 import fr.unice.polytech.si3.qgl.iaad.util.resource.Resource;
+import fr.unice.polytech.si3.qgl.iaad.util.contract.SecondContract;
 import org.json.JSONObject;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Théo on 23/02/2017.
@@ -18,40 +23,37 @@ import static org.junit.Assert.assertTrue;
 public class SecondContractTest {
 
     Context context;
-    JSONObject firstContext;
     SecondContract secondContract;
+    private List<Contract> contracts;
 
     @Before
-    public void defineContext() {
-
-        firstContext = new JSONObject("{ \n" +
-                "  \"men\": 12,\n" +
-                "  \"budget\": 10000,\n" +
-                "  \"contracts\": [\n" +
-                "    { \"amount\": 600, \"resource\": \"WOOD\" },\n" +
-                "    { \"amount\": 200, \"resource\": \"GLASS\" }\n" +
-                "  ],\n" +
-                "  \"heading\": \"W\"\n" +
-                "}");
-        // context = new Context(firstContext);
+    public void setUp() throws Exception
+    {
+        context = mock(Context.class);
+        contracts = new ArrayList<>();
+        contracts.add(new StandardContract(Resource.FISH, 1000));
+        contracts.add(new StandardContract(Resource.GLASS, 50));
+        when(context.getBudget()).thenReturn(10000);
+        when(context.getHeading()).thenReturn(Direction.E);
+        when(context.getNumberOfMen()).thenReturn(12);
+        when(context.getContracts()).thenReturn(contracts);
         secondContract = new SecondContract(context);
         this.secondContract.createSecondContract();
     }
 
-    @Ignore
+
     @Test
     public void createSecondContractTest(){
         Set listKeys=this.secondContract.getSecondContract().keySet();
         Iterator iterateur=listKeys.iterator();
-        while(iterateur.hasNext())
+        for (int i=0;i<secondContract.getSecondContract().size();i++)
         {
             assertTrue(Resource.isPrimary((Resource) iterateur.next()));
         }
-        assertTrue(this.secondContract.getSecondContract().get(Resource.WOOD)==1600);
-        assertTrue(this.secondContract.getSecondContract().get(Resource.QUARTZ)==2000);
+        assertTrue(this.secondContract.getSecondContract().get(Resource.FISH)==1000);
+        assertTrue(this.secondContract.getSecondContract().get(Resource.QUARTZ)==500);
     }
 
-    @Ignore
     @Test
     public void createSecondContractToBeCraftedTest(){
         Set listKeys=this.secondContract.getToBeCrafted().keySet();
@@ -60,7 +62,7 @@ public class SecondContractTest {
         {
             assertTrue(!Resource.isPrimary((Resource) iterateur.next()));
         }
-        assertTrue(this.secondContract.getToBeCrafted().get(Resource.GLASS)==200);
+        assertTrue(this.secondContract.getToBeCrafted().get(Resource.GLASS)==50);
     }
 }
 
