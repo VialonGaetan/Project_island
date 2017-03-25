6c7fb461cf1634d0e1684e99d0bff5c75c2f8908
romain <romain.sumerot@etu.unice.fr>
2017-03-25 18:17:49 +0100

    added tests in AerialTest

--- src/test/java/fr/unice/polytech/si3/qgl/iaad/strategy/advanced/aerial/ReturnToIslandTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/iaad/strategy/advanced/aerial/ReturnToIslandTest.java
@@ -1,7 +1,19 @@
 package fr.unice.polytech.si3.qgl.iaad.strategy.advanced.aerial;
 
+import fr.unice.polytech.si3.qgl.iaad.util.map.Compass;
+import fr.unice.polytech.si3.qgl.iaad.util.map.Direction;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 /**
- * Created by romain on 20/03/17.
+ * @author romain
+ * Created on 20/03/17.
  */
-public class ReturnToIslandTest {
+public class ReturnToIslandTest
+{
+    private ReturnToIsland returnToIsland;
 }
