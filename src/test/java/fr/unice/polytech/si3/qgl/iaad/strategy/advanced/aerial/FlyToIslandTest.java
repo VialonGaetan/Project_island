a6e52f4e6f0f131a42d84220061d5c567a6e90cc
romain <romain.sumerot@etu.unice.fr>
2017-03-25 19:27:47 +0100

    Copy class tests from dev

---
+++ src/test/java/fr/unice/polytech/si3/qgl/iaad/strategy/advanced/aerial/FlyToIslandTest.java
@@ -0,0 +1,25 @@
+package fr.unice.polytech.si3.qgl.iaad.strategy.advanced.aerial;
+
+import fr.unice.polytech.si3.qgl.iaad.util.map.Compass;
+import fr.unice.polytech.si3.qgl.iaad.util.map.Direction;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * @author romain
+ * Created on 20/03/17.
+ */
+public class FlyToIslandTest
+{
+    private FlyToIsland flyToIsland;
+
+    @Test
+    public void stayOnMap()
+    {
+
+    }
+}
